44cb6450283d67fba0be2b0aa5c4bd015555a55d
DCisHurt <roger33669@gmail.com>
2025-02-11 10:34:19 +0000

    update the threshold logic

--- calibration.py
+++ calibration.py
@@ -6,19 +6,20 @@ def beambreak_calibration(beambreak: AnalogPort):
 
     MAX_DUTY = 57500
     MIN_DUTY = 27000
+    OFFSET = 1200
     pwm = 0
     beambreak.setGain(1)
     beambreak.setPWM(0)
     for duty in range(MIN_DUTY, MAX_DUTY, 250):
+        pwm = duty
         beambreak.setPWM(duty)
         time.sleep_ms(1)
         if beambreak.value() > MIN_DUTY:
-            pwm = duty
             break
     gain = MAX_DUTY / beambreak_avg(beambreak)
     beambreak.setGain(gain)
 
-    threshold = round(beambreak_avg(beambreak) * 1.04)
+    threshold = beambreak_avg(beambreak) + OFFSET
 
     return threshold, gain, pwm
 
